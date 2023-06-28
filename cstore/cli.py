4e9fc1c727fdfd258d560e208c73da0a99ed2b84
Navid Arabi <navidved@gmail.com>
2023-06-29 01:31:40 +0330

    order results

--- cstore/cli.py
+++ cstore/cli.py
@@ -399,7 +399,7 @@ def main(
             is_secret=secret)
 
         if not entities:
-            input_prompt = Prompt.ask("Please enter a search term:")
+            input_prompt = Prompt.ask("Please enter a search term")
             entities = schemes.EntitiesSchema(
                 command=schemes.CommandSchemaBase(
                     description=description,
